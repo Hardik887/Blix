c629ec9de9d0e989b4b7a37338b7f2572b7cdb04
Hardik <devil.778@Baljeets-MacBook-Air.local>
2022-01-22 15:22:57 -0500

    Final Fix

--- src/requests.js
+++ src/requests.js
@@ -2,7 +2,7 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const API_KEY = process.env.REACT_API_KEY;
+const API_KEY = "5b224ad99eaceb26bbb2e3602d6e9f2b";
  
 const requests = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-us`,
